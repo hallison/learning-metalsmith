97e5ad2c8476f32c8cda7fcab53fbf6e6c49f535
Hallison Batista <hallisonbatista@bemol.com.br>
2021-06-18 19:26:59 -0400

    Plugin para layouts

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 const metalsmith = require("metalsmith");
 const markdown = require("metalsmith-markdown");
+const layouts = require("metalsmith-layouts");
 
 const package = require("./package.json");
 
@@ -29,6 +30,10 @@ site.metadata({
 site.source("./content");
 site.destination("./deploy");
 site.clean(true);
+
+site.use(layouts({
+  default: "default.ejs",
+}));
 site.use(markdown());
 
 site.build(checkFail);
